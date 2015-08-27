b910dc9b77bb49fce760d5cc00483eaadd24380c
TonyDon <hongdeviltxd@163.com>
2015-08-27 13:42:39 +0800

    update

--- src/test/java/com/uuola/weixin/AppTest.java
+++ src/test/java/com/uuola/weixin/AppTest.java
@@ -1,38 +1,46 @@
 package com.uuola.weixin;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.uuola.commons.http.HttpClientFactory;
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+    
+    CloseableHttpClient httpClient = null;
+    
+    @Before
+    public void init(){
+        httpClient = HttpClientFactory.getHttpClient();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void test_BaiduApi_1() throws ClientProtocolException, IOException{
+        HttpGet get = new HttpGet("http://www.baidu.com");
+        CloseableHttpResponse response = httpClient.execute(get);
+        System.out.println(response.getStatusLine());
+        System.out.println(EntityUtils.toString(response.getEntity()));
     }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    
+    @After
+    public void finish(){
+        try {
+            httpClient.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
